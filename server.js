d82ac4afb913f14d310e4c9574351f87acd03554
science <petroff.ryan@gmail.com>
2018-09-22 00:17:03 -0400

    added SQLite db

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const port = 3000;
 var router = express.Router();
 var path = __dirname + '/views/';
 
+const sqlite3 = require('sqlite3').verbose();
+
+// open database connection
+let db = new sqlite3.Database('./db/problems.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the problem database.');
+});
+
+
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
@@ -29,4 +41,13 @@ app.use("*",function(req,res){
 
 app.listen(port,function(){
   console.log("Live at Port 3000");
+});
+
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
 });
